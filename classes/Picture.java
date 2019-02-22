b8c5832206365295d8e8bf915bd9ae2db37ba170
Jasper <jbull20@dtechhs.org>
2019-02-22 10:11:18 -0800

    trying to submit to gradescope

--- classes/Picture.java
+++ classes/Picture.java
@@ -385,17 +385,42 @@ public class Picture extends SimplePicture
           leftPixel.setColor(Color.WHITE);
       }
     }
-    for (int row = 0; row < pixels.length-1; row++) {
-        for (int col = 0; col < pixels[0].length; col++) {
-            topPixel = pixels[row][col];
-            bottomPixel = pixels[row+1][col];
-            bottomColor = bottomPixel.getColor();
-            if(topPixel.colorDistance(bottomColor) > edgeDist)
-                topPixel.setColor(Color.BLACK);
-            else
-                topPixel.setColor(Color.WHITE);
-        }
-    }
+  }
+
+  public void edgeDetection2(int edgeDist) {
+      Pixel leftPixel = null;
+      Pixel rightPixel = null;
+      Pixel topPixel = null;
+      Pixel bottomPixel = null;
+      Pixel[][] pixels = this.getPixels2D();
+      Color rightColor = null;
+      Color bottomColor = null;
+      for (int row = 0; row < pixels.length; row++)
+      {
+          for (int col = 0;
+               col < pixels[0].length-1; col++)
+          {
+              leftPixel = pixels[row][col];
+              rightPixel = pixels[row][col+1];
+              rightColor = rightPixel.getColor();
+              if (leftPixel.colorDistance(rightColor) >
+                      edgeDist)
+                  leftPixel.setColor(Color.BLACK);
+              else
+                  leftPixel.setColor(Color.WHITE);
+          }
+      }
+      for (int row = 0; row < pixels.length-1; row++) {
+          for (int col = 0; col < pixels[0].length; col++) {
+              topPixel = pixels[row][col];
+              bottomPixel = pixels[row+1][col];
+              bottomColor = bottomPixel.getColor();
+              if(topPixel.colorDistance(bottomColor) > edgeDist)
+                  topPixel.setColor(Color.BLACK);
+              else
+                  topPixel.setColor(Color.WHITE);
+          }
+      }
   }
   
   
